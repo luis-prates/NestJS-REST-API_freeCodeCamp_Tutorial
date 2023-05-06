fcccb4d9988bb0a669a2865bbe4c4dc9ba06372e
Luis Prates <lprates@student.42lisboa.com>
2023-05-06 23:51:30 +0100

    coors configure

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.enableCors();
+	app.enableCors({
+		origin: '*',
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	});
 	app.useGlobalPipes(new ValidationPipe({
 		whitelist: true
 	}))
